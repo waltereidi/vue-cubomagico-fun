ca60db786b5b440be158fc33908dc3f9274aa118
waltereidi <walter-eidi@hotmail.com>
2023-09-15 21:08:23 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<script >
+export default {
+  data() {
+    return {
+      showElement: true,
+    }
+  },
+  methods: {
+    change() {
+      setTimeout(async () => {
+        this.showElement = !this.showElement;
+        this.change();
+      }, 500)
+    }
+  },
+  mounted() {
+    this.change();
+  }
+}
+
+</script>
+
+<template>
+  <div class="content">
+    <div class="boxshadows">
+
+
+    </div>
+    <div class="gradient" v-if="showElement">
+      <div class="grid">
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+      </div>
+    </div>
+    <div class="gradient" v-if="!showElement">
+      <div class="grid">
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+        <div class="grid-item"
+          :style="{ 'background-color': 'color(display-p3 ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ' ' + Math.random().toFixed(20) + ')' }">
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+@import "./assets/main.scss";
+</style>
